d4779e1f567519d7fdc7da3ff0a9ee1010b3fbf8
tsherlock86 <tadsherlock@gmail.com>
2015-08-21 09:41:40 -0400

    Fixes to shopping cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ActiveRecord::Base
   include Payola::Sellable
 
+  has_many :itemcarts
+  has_many :carts, through: :itemcarts
+  belongs_to :user
+
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  
+  has_many :products
+  has_one :cart
 
 
 
