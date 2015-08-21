01a9e34fed3cb291238ffc49a38bf48fd73d261f
Dallie Hill <lenore776@gmail.com>
2015-08-21 10:33:17 -0400

    Working on Refile and images

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
   include Payola::Sellable
+  permalink :name
+  attachment :file
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820184322) do
+ActiveRecord::Schema.define(version: 20150821134835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,6 +121,7 @@ ActiveRecord::Schema.define(version: 20150820184322) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "permalink"
+    t.string   "file"
   end
 
   create_table "users", force: :cascade do |t|
