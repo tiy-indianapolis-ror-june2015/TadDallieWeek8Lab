2ad2d245373a492843959b14bb59f6339071fea1
Dallie Hill <lenore776@gmail.com>
2015-08-22 14:40:15 -0400

    "Worked on seed file and search ability"

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,10 @@ class Product < ActiveRecord::Base
   has_many :carts, through: :itemcarts
   belongs_to :user
 
+  def self.search(query)
+    where ("name like ?", "%#{query}%")
+    where ("description like ?", "%#{query}%")    
+  end
 
 
 end
