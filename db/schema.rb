166287f241b0e054a7a8d400e3a783317857d2b6
Dallie Hill <lenore776@gmail.com>
2015-08-24 13:25:29 -0400

    Working on S3 images

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150824005723) do
+ActiveRecord::Schema.define(version: 20150824163044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,11 +20,6 @@ ActiveRecord::Schema.define(version: 20150824005723) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "permalink"
-  end
-
-  create_table "itemcarts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "line_items", force: :cascade do |t|
@@ -140,7 +134,7 @@ ActiveRecord::Schema.define(version: 20150824005723) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "permalink"
-    t.string   "file"
+    t.string   "file_id"
   end
 
   create_table "users", force: :cascade do |t|
